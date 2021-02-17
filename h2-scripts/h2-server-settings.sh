2063a1f7d2ea2e763c3c0d7da49e78fcd88168bb
James Rice <James.Rice@ttg-solutions.com>
2021-02-17 14:01:43 -0500

    corrected bash script from windows batch version.

--- h2-scripts/h2-server-settings.sh
+++ h2-scripts/h2-server-settings.sh
@@ -1,4 +1,4 @@
-@echo off
+#!/bin/bash
 # Quick way to see the h2 command-line parameters.
 
 # The h2 jar is populated via the maven dependency plugin to the $project\h2 folder.
@@ -9,11 +9,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
 # Location and name of the h2 jar.  Run mvn dependency:copy or mvn package to have the h2 jar copied to the h2 folder.
-set H2_JAR_PATH=${DIR}/../h2
-set H2_JAR_NAME=h2-1.4.200.jar
+H2_JAR_PATH=${DIR}/../h2
+H2_JAR_NAME=h2-1.4.200.jar
 
-echo.
+echo
 
 java -cp ${H2_JAR_PATH}/${H2_JAR_NAME} org.h2.tools.Server -?
-echo.
+echo
 
